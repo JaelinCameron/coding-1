441ef44d2865d3cfaa4cd203495cdeb54f77b202
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2024-10-29 13:35:06 +0000

    Python Notes

---
+++ classNotes/pythonDataTypes.py
@@ -0,0 +1,34 @@
+# Strings 
+# srings are pieces 
+
+
+
+
+
+# Intagers 
+# Intagers are pieces if data that represent whole numbers.
+# whole nubmers do not have decimals 
+22
+45
+2542564453256
+
+# Floats
+# Floats represent pieces of data that contain a decimal 
+33.56
+1342.43
+00.387
+
+# booleans
+# 
+True
+False
+# varibles 
+# varibles are containers for data that can be re-used
+username="JaelinCameron"
+password=''
+profileImg=''
+post=[]
+
+{},
+[],
+(),
