1a610b8aaf3292b7e98290b7680f651cd35b8790
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2024-10-31 13:36:14 +0000

    Python foundation

---
+++ Unit3_python/pythonFoundation.py
@@ -0,0 +1,40 @@
+# data types= basic building block of python programming
+
+# string = data represented as characters wrapped
+# together in quotation marks 
+"ryefhuer"
+# intagers = whole numbers.
+678
+
+# floats = decimal numbers.
+697.99
+
+# boolean = true or false values 
+True
+False
+
+
+# varibles = containers for data 
+# we create a varible using the following steps
+# 1. create a varible name
+# 2. apply a equal sign
+# 3. give it a data value
+
+user = "Jaelin"
+message ='Good Morning'
+weather ="It is 60 degress and sunny."
+
+# we use print to see data in our terminal
+print(message + ", " + user + ", " + weather) 
+
+number1=1200
+number2=800
+
+print(number1, number2)
+print(number1 + number2)
+print(number1 - number2)
+
+name = 'my name is Jaelin'
+activity ='I am going to go cook at my job'
+time ='I will do this at 5:00pm'
+print(name +", " + activity + ", " + time)
