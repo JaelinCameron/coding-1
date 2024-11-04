8597129ecdc39fc8de80b31218ac82273448f504
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2024-11-04 14:05:26 +0000

    Async acvity nov1

---
+++ unit 2/asyncActivity_Nov1.py
@@ -0,0 +1,9 @@
+number1=200
+number2=45
+print(number1 + number2)
+
+# If I were asked to make a calander app I would use the varible data type
+# For the date I would use the interger data type
+# The Interger is all whole numbers and floats are numbers with decimals
+# the data type is boolian and print
+# The three things I would like to code faster, to know coding better, and make quality code
