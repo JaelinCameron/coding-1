aa16948de8f87d8f8d8004b3cb45812ed2f9d045
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2024-11-07 14:33:58 +0000

    Python Data Operators Notes

--- classNotes/pythonDataTypes.py
+++ classNotes/pythonDataTypes.py
@@ -32,3 +32,52 @@ post=[]
 {},
 [],
 (),
+
+# Varibles- simply put, containers
+# A varible name can not start with a number 
+# Every new starts with a capital letter
+# The letters are supposed to represent the camels hump
+
+homeaddress= "123 Main Street"
+telephonenumber= "2345654534"
+telephoneBillPaid= True
+telephoneBillPaid= False
+balance= 300.00
+
+# snake case = every word is seperated by an underscore
+# The underscore is supposed to represent a snake
+celluarCode_byUser_state_resetPassword= '23wedfghjki8u76'
+
+# parscal case every word is capitalized
+# pascal is a name so it mimics First name Last name capitalization
+FamilyDiscountCode25 = .25
+FamilyDiscountCode50 = .50
+FamilyDiscountCode100 = 1.00
+
+# Python Operators
+# Arithmetic Operators = used to perform common mathematical operation
+p= 20
+s=10
+print(p+s)
+print(p-s)
+print(p/s)
+
+# Assignment Operators = used to assign values
+x= 5
+x= x+3 # Assigning the value x and then adding 3
+x= x-3
+x= x^3
+x= x/3
+
+print(x)
+
+# Comparison operators
+
+# The Equal sign in python has nothing to do with math 
+# #Assigning a value Not geting a resualt
+# double equal sign (==) is used for comparison
+
+valueA= "Jaelin"
+valueB= "Jaelin"
+
+print(valueA == valueB)
--- unit 2/asyncActivity_Nov1.py
+++ unit 2/asyncActivity_Nov1.py
@@ -6,4 +6,4 @@ print(number1 + number2)
 # For the date I would use the interger data type
 # The Interger is all whole numbers and floats are numbers with decimals
 # the data type is boolian and print
-# The three things I would like to code faster, to know coding better, and make quality code
+# The three things I would like to code faster, to know coding better, and make quality code 
