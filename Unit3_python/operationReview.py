5e894792f1ea1d0046adb84df5535d0fc7cda196
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2024-11-13 14:35:46 +0000

    We reviewed 4 Python Operators

---
+++ Unit3_python/operationReview.py
@@ -0,0 +1,58 @@
+# arithmetic operator - writing code that does math processes
+deposit_amount = '50'
+widthdraw_amount = '25'
+checking_acount = '1000'
+saving_acount = '2000'
+creditamount = '1500'
+paypalfee = '4'
+
+print(deposit_amount + checking_acount)
+
+parlayamount =''
+
+# assignment operators - giving varibles values. KEYWORD is assignment
+# any time you're using a varible, you are using the assignment operator
+
+x= 5
+x += 3
+print(x)
+
+teeShirt= 50.00
+
+discount= .25
+
+appliedDiscount = teeShirt *discount
+total= teeShirt -appliedDiscount
+
+print(total)
+
+# Comparison operator- analyzing two values to determine a outcome.
+# == this means they are the same or equal
+# != this means they are NOT the same or equal
+
+wendysReviewScore = 4.0
+burgerKingReviewScore = 4.2
+
+wendyBurgerCalories = 200
+burgerKingBurgerCalories = 300
+
+dailyCaloricIntake = 3000
+
+ApplebeesMeal= 2300
+
+# compare best reviewed resteraunt
+print(burgerKingReviewScore > wendysReviewScore)
+print(burgerKingReviewScore < wendysReviewScore)
+
+# Capturing caloric intake
+print(burgerKingBurgerCalories > wendyBurgerCalories)
+
+# can I eat this meal today
+print(dailyCaloricIntake == ApplebeesMeal)
+print(dailyCaloricIntake != ApplebeesMeal)
+
+# 1 equal sign is used only for assigning values
+# 2 equal signs are used for comaparing 
+
+# logical operator
+
