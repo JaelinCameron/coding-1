f92ec2e7c08e31f19e49aa1c2f18076a836f1204
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2024-12-02 14:11:42 +0000

    Pyhton Functions Notes

--- Unit3_python/functions.py
+++ Unit3_python/functions.py
@@ -13,21 +13,20 @@ print('Coding Class')
 
 # input () is a built in function. It allows us to write and pass 
 #dra into our program from the terminal
-name= input("what is your name?")
-print(name)
+#name= input("what is your name?")
+#print(name)
 
-number= int(input('please provide your pin number'))
-print(23^number)
+#number= int(input('please provide your pin number'))
+#print(23^number)
 
 # User Defined function- custom functions written by engineers
 
 # Function Syntax- how it is written
 # def is short for define
 # this is a function definition
-def sandwitcheInstructions():
-    print("step 1. get 2 pieces of bread,")
-    print("step 2. putting ingredients inside of bread,")
-    print("step 3. put bread with ingredients together,")
+#def sandwitcheInstructions():
+    #print("step 1. get 2 pieces of bread,")
+    #print("step 3. put bread with ingredients together,")
 
 # there are two states of a user defined function;
     # the function defintion
@@ -39,28 +38,55 @@ def sandwitcheInstructions():
 
 # fumnctions using arithmitic operators
 
-def depositMoney(x):
-   checkingAccount =100
-   print('Money has been deposited successfully')
-   print('new checking account balance is below: ')
-   print(x + checkingAccount)
-depositMoney(35)
-depositMoney(300)
-depositMoney(195)         
+#def depositMoney(x):
+ #  checkingAccount =100
+  # print('Money has been deposited successfully')
+   #print('new checking account balance is below: ')
+   #print(x + checkingAccount)
+#depositMoney(35)
+#epositMoney(300)
+#depositMoney(195)         
 
 
-def withdrawMoney(x):
-    checkingAccount =1000
-    print('Your Money has been withdrawed successfully')
-    print('Your new balance is below')
-    print(checkingAccount - x)
-withdrawMoney(5)
-withdrawMoney(403)
-withdrawMoney(67)
+#def withdrawMoney(x):
+ #   checkingAccount =1000
+ #   print('Your Money has been withdrawed successfully')
+ #   print('Your new balance is below')
+ #   print(checkingAccount - x)
+#withdrawMoney(5)
+#withdrawMoney(403)
+#withdrawMoney(67)
 
-def checkAccountBalance():
-    checkingAccount =2000
-    print('here is current check account balance')
-    print(checkingAccount)
+#ef checkAccountBalance():
+    #checkingAccount =2000
+    #print('here is current check account balance')
+    #print(checkingAccount)
 
 # def transferMoney()
+
+
+
+
+
+
+
+
+# built-in functions
+"The instruction for out function is already written"
+
+#print()
+#input()
+
+#int()
+#str()
+#float()
+
+# user- defined functions
+"We as enhineers write the instructions for our program step by step"
+
+def welcomeMessage():
+    name = input("Please type your name ")
+    print("Hello, Welcome back to school, " + name)   
+
+
+welcomeMessage()
