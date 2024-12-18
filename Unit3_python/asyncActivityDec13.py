14b861a46a8643009794614c87719591e92ddf6e
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2024-12-18 14:35:25 +0000

    Async dec 13 review

--- Unit3_python/asyncActivityDec13.py
+++ Unit3_python/asyncActivityDec13.py
@@ -20,4 +20,35 @@ def netflixAge(Age):
     elif Age >= 20:
         print("You have to pay $15.00")
 
-netflixAge(20)
+netflixAge(30)
+netflixAge(2)
+netflixAge(16)
+netflixAge(67)
+
+# The question past this point is the question I could not finish
+
+def discountShoping(membership, itemPrice):
+    if membership == 'superShopper':
+        print('you are getting 10 percent off')
+        discount= itemPrice * .1
+        total= itemPrice - discount
+        print(discount)
+    elif membership ==  'megaShopper':
+        print('you are getting 15 percent off')
+        discount= itemPrice * .15
+        total= itemPrice - discount
+        print(discount)
+    elif membership == 'ultraShopper':
+        print('you are getting 20 percent off')
+        discount= itemPrice * .2
+        total= itemPrice - discount
+        print(discount)
+    else:
+        print('Erorr: sorry, that membership type dosent exist')
+
+discountShoping('superShopper', 100)
+discountShoping('megaShopper', 100)
+discountShoping('ultraShopper', 100)
+discountShoping('alphaShopper', 100)
+
+
