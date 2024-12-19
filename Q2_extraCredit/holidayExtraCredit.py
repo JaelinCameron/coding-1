376c70bcaeedefe0ccad48361cab70c8c140f882
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2024-12-19 14:30:36 +0000

    Holiday extra credit 12/19/2024

---
+++ Q2_extraCredit/holidayExtraCredit.py
@@ -0,0 +1,27 @@
+def schedulePlanner(month, day, year):
+    if month == 12:
+        if day == 21:
+            if year == 2024:
+                print('Winter break has started, relax.')
+    if month == 12:
+            if day == 22:
+                if year == 2024:
+                    print('Go christmas shoping.')
+    if month == 12:
+            if day == 24:
+                if year == 2024:
+                    print('It is christmas eve.')    
+    if month == 12:
+            if day == 25:
+                if year == 2024:
+                    print('It is christmas!')
+    if month == 1:
+            if day == 7:
+                if year == 2025:
+                    print('Winter break is over!')  
+    else:
+         print("nothing is planned for this day.")    
+
+
+schedulePlanner(12, 22, 2024)
+                     
