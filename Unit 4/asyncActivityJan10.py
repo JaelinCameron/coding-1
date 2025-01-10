f0215f06e1add188fe2ae360a1a0d10c1433363b
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2025-01-10 14:17:19 +0000

    Async Activity Python Loops Jan10

---
+++ Unit 4/asyncActivityJan10.py
@@ -0,0 +1,28 @@
+listOfFrozenFood = ['Frozen Food aisle',
+              'Yogurt'
+              'Frozen Pizza',
+              'Waffles',
+              'Microwave Dinner'
+              ]
+
+listOfInternationalFood = ['International Food aisle',
+                            'Poutine',
+                            'Samosa'
+                            'Shepherds Pie'
+                            'Falafel and Pita'
+                            'Carbonara.'
+                           ]
+
+listOfProduce = ['Produce food',
+                 'Onion',
+                 'Orange',
+                 'Apple',
+                 'Beats',
+                 'Potato'
+                 ]
+
+numbers = [1,30,39,50,293,100]
+print(numbers[0])
+print(numbers[3])
+print(numbers[4])
+print(numbers[4]* 2)
