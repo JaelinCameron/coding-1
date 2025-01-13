64be0f6a871b90099cd45b07c879fec16f9c7e37
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2025-01-13 14:11:10 +0000

    Reviewing async work (Already did it)

---
+++ Unit 4/asyncActivityJan10Review.py
@@ -0,0 +1,13 @@
+def travelPlces():
+# input is a built-in function that allows us to pass in data 
+# from the terminal and assign it ot a varible
+
+    # built-in function - a function thats built into the programing
+    # language that we dont need to write instructions for.
+    # the instruction are already written behind the scenes, we just 
+    # need to call it 
+     
+    place = input('where have you traveled to?')
+    # When we answer the question above, the data we give the program
+    # will be assigned to the variable  
+    print(place)              
