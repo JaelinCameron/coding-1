71dec31dc5fe1dbeca303a6d409afaae389e74d1
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2025-01-29 14:36:03 +0000

    Problem solving notes

---
+++ classNotes/problemSolvingTechniques.py
@@ -0,0 +1,26 @@
+# Challenges in software development are:
+# Working with legacy code (old code)
+# Developing a new piece of code to satisfy a specific feature
+# Managing many peieces of code to work together
+
+# How to Problem Solve
+
+# Step 1: Read and Listen to understand
+# Boil down the question to the key points to save time and trouble
+# Make sure to take down notes of your thoughts
+
+# Step 2: Locate keywords and phrases
+# In most questions the answer is in it 
+# Try to think about the context of the word as in "when" "here" and "how" you saw this word
+
+# Step 3: Is there an input(data) in the question
+# Does the question give you data to work with?
+
+# Step 4: Does the problem require an output(data)
+# Does the problem ask you to provide data as a result 
+
+# Step 5: Pseudo Code your notes
+# Pseudo code is a way of planning how you would solve the coding problem 
+# When writing psuedo code your goal is to simply write down the steps you would take to 
+# solve the problem but within the context of the coding language
+# You are not actually writing the actual functional code when writing psuedo code
