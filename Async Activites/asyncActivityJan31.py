fc0aaf7a84b4791f98725ac171d8333be0ef217a
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2025-01-31 14:36:26 +0000

    async problem solving

---
+++ Async Activites/asyncActivityJan31.py
@@ -0,0 +1,70 @@
+# Boys Latin has hired you to develop a grade checker program for students. 
+# Your program should be able to do the following; allow a student to enter a numerical grade, 
+# then return the appropriate letter grade. For example; if a student enters the number 80, 
+# the program should give back the letter grade "B".
+# Your program should then repeat by  asking the user to enter another numerical grade.
+# Each time a user enters another numerical grade, it should perform some calculation 
+# to average out the grades and show the new letter grade, 
+# For example: if the user enters the number 70, and the 100,
+# they're average should be an 85, which is a B.
+
+# 1. In a complete sentence, what is the problem asking you to do ?
+# The problem is asking me to make a function where students can input a number and output a 
+# letter appropriate to the grade.
+
+# 2. Where there any parts of the question that don't make sense, if so write them down in complete sentences. 
+# Im having difficulty with pieceing together how to code a system that will give
+# a grade point average 
+
+# 3. Please write down the keywords and phrases that will help you solve this problem ? 
+# Numerical grade
+# Letter grade
+# Calculation
+
+# 4. Does the problem provide you with any input data? Does it tell you what type of data your program needs to take in ?
+# It tells to input a number; a numerical grade
+
+# 5. Does the problem provide you with desired output data? Does it tell you that your program needs to give somthing back to the user ? 
+# It tells you to out put a letter around the range of the appropriate
+
+# 6. Using Pseudocode (fake code), write down Step by step how you think you can solve this problem. Remember, this does not have to be code.  
+# Make a function 
+# Use If else conditional statements.
+# print()
+# Arithmatic operator
+
+
+
+
+# You are working on a game concept with several other engineers
+#  and you need to provide them with a simple proof of concept.
+#  For now, you have suggested that your game work on a RPS (rock, paper, scissors) loop.
+#  When a user plays, rock, paper, or scissors, the computer will randomly 
+#  select one of the 3 options as well. Based on the results of a match,
+#  Your program should conditionally determine who won, loss, or if it
+#  was a draw based on the values. The game should only allow the player 
+# to play 3 for 3 turns, and inform the player if the won or lost that match at the end
+#  (this RPS game is a best of 2 out of 3 format.)
+
+#  1. What is the problem asking you to do?
+# The problem is asking me to make a program that randomly chooses between Rock, Paper, or scissors
+# that also outputs the player's amount of wins
+
+# 2. What are some keywords and phrases ? 
+# Randomly selects
+# 3 hands
+
+
+# 3. Is there input data that the question provides ?
+# We need to be able to put in either RPC
+
+
+# 4. What is the desired output ?
+# If the player wins or lose 
+
+
+# 5. Pseudocode Steps & Additional Notes 
+# import Random 
+# random.choice
+# while loop
+# if else conditionals 
