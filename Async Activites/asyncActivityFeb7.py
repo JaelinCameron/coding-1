e12f14cd4f5783783fde7df1d9b9083a61df66fa
Jaelin Cameron <119974591+JaelinCameron@users.noreply.github.com>
2025-02-07 14:28:03 +0000

    async  feb 7

---
+++ Async Activites/asyncActivityFeb7.py
@@ -0,0 +1,13 @@
+# A FOR loop is a type of loop that iterates over a list and 
+# a WHILE loop repeats as long as a condition is true
+
+# You would use a WHILE loop in a inventory system
+# the loop would continue as long as your inventory if not full.
+ 
+def highestgrade():
+    Grades= [50, 67, 39, 98, 80, 93]
+    x=  max(Grades)
+    print (x)
+    print('is the highest number')
+
+highestgrade()
